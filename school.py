d6124179bc0dc911736d50499b135b3e29377752
Botafogo1894 <pruchka@gmail.com>
2018-10-29 15:15:06 -0400

    PArt 5 done

--- school.py
+++ school.py
@@ -10,8 +10,22 @@ class School:
         except:
             self._roster[grade] = [name]
 
+    def grade(self, grade):
+        return self._roster[grade]
+
+    def sort_roster(self):
+        new_roster = self._roster.copy()
+        for key in new_roster.keys():
+            new_roster.update({key: sorted(new_roster[key])})
+        return sorted(new_roster.items(), key = lambda item: item[0])
+
+
 school = School("Middletown High School")
 
 school.add_student("Dilyan", 12)
+school.add_student("Kelly Slater", 9)
+school.add_student("Tony Hawk", 10)
+school.add_student("Ryan Sheckler", 10)
+school.add_student("Bethany Hamilton", 11)
 
 print(school._roster)
